179597833a4e300087652c1be628b2e62b223e3a
MilanBom <milan.bom@student.uva.nl>
2024-06-13 19:27:05 +0200

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 13 10:06:42 2024
+Created on Thu Jun 13 17:40:00 2024
 
 @author: milan
 """
 
+from astropy.wcs import WCS
 import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
@@ -13,10 +14,8 @@ import matplotlib.pyplot as plt
 import os  # os.path to manipulate file paths
 import glob  # finding pathnames (to search for certain fits files in folders and subfolders)
 
-
-
 # Path to the FITS file to process
-data_im = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Stack van alle datasets/GedownloadHa.fits'
+data_im = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Stack van alle datasets/GedownloadO3.fits'
 
 try:
     # Open the FITS file and extract data
@@ -24,54 +23,38 @@ try:
         hdu = hdu_list[0]
         header = hdu.header
         data = hdu.data
-        
-    true_wcs = WCS(header)
-    print(header)
+ 
+    wcs = WCS(header)
 
-    # Plot the FITS image with WCS coordinates
-    plt.figure()
-    #ax = fig.add_subplot(projection=true_wcs)
-
+    wx1, wy1 = wcs.wcs_world2pix(313.9,30.87, 1)
+    wx2, wy2 = wcs.wcs_world2pix(314.5,31.34, 1)
+    
+    
+    fig = plt.figure()
+    ax = plt.subplot(111, projection=wcs)
+    
     # Apply ZScaleInterval to set the display range
     z = ZScaleInterval()
     z1, z2 = z.get_limits(data)
-
-    x_center = header['CRVAL1']
-    y_center = header['CRVAL2']
     
-    # Define the provided pixel scale in degrees
-    pixel_scale = 0.431 / 3600
-
-    left = x_center - (header['CRPIX1'] * pixel_scale)
-    lower = y_center - (header['CRPIX2'] * pixel_scale)
+    ax.imshow(data, cmap='gray', vmin=z1, vmax=z2, interpolation=None, origin='lower')
+    ax.set_xlabel("Right Ascension [degrees]")
+    ax.set_ylabel("Declination [degrees]")
+    ax.coords.grid(color='white', alpha=0.5, linestyle='solid')
     
-    right = left + (4096*pixel_scale)
-    top = lower + (4096*pixel_scale)
+    ax.set_autoscale_on(False)
+    ax.set_xlim(wx1,wx2)
+    ax.set_ylim(wy1,wy2)
     
-    extent_image = [left, right, lower, top]
-
-    plt.imshow(data, vmin=z1, vmax=z2, cmap='viridis', origin='lower', extent=(left, right, lower, top))
-    #plt.set_title('FITS Imagewith WCS Coordinates')
+    plt.plot
     
-    plt.xlabel('RA')
-    plt.ylabel('Dec')
-    
-    plt.ylim(top ,lower)
-    plt.xlim(left, right)
-    # Show plot
-    plt.show()
-    print(left)
-    print(right)
-    print(lower)
-    print(top)
-
 except FileNotFoundError:
     print(f"File {data_im} not found.")
 except ValueError as ve:
     print(f"ValueError: {ve}")
 except Exception as e:
     print(f"An error occurred: {e}")
-
+    
 # Import list of fits files:
 # Set path to calibration folder (path_in) and path to save files (path_out)
 path_in = "C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen"
@@ -93,35 +76,26 @@ for getal in range(0,3):
     header = hdu_list.header
     data, true_wcs = hdu_list.data, WCS(hdu_list.header)
 
-    # Plot the FITS image with WCS coordinates
-    plt.figure()
-    #ax = fig.add_subplot(projection=true_wcs)
+    wcs = WCS(header)
 
+    wx1, wy1 = wcs.wcs_world2pix(313.9,30.87, 1)
+    wx2, wy2 = wcs.wcs_world2pix(314.5,31.34, 1)
+    
+    
+    fig = plt.figure()
+    ax = plt.subplot(111, projection=wcs)
+    
     # Apply ZScaleInterval to set the display range
     z = ZScaleInterval()
     z1, z2 = z.get_limits(data)
     
-    x_center = header['CRVAL1']
-    y_center = header['CRVAL2']
+    ax.imshow(data, cmap='gray', vmin=z1, vmax=z2, interpolation=None, origin='lower')
+    ax.set_xlabel("Right Ascension [degrees]")
+    ax.set_ylabel("Declination [degrees]")
+    ax.coords.grid(color='white', alpha=0.5, linestyle='solid')
     
-    # Define the provided pixel scale in degrees
-    pixel_scale = 1.76 / 3600
-
-    left = x_center - (header['CRPIX1'] * pixel_scale)
-    lower = y_center - (header['CRPIX2'] * pixel_scale)
+    ax.set_autoscale_on(False)
+    ax.set_xlim(wx1,wx2)
+    ax.set_ylim(wy1,wy2)
     
-    right = left + (1024*pixel_scale)
-    top = lower + (1024*pixel_scale)
-    
-    extent_image = [left, right, lower, top]
-
-    plt.imshow(data, vmin=z1, vmax=z2, cmap='viridis', origin='lower', extent=(left, right, lower, top))
-    #plt.set_title('FITS Imagewith WCS Coordinates')
-    
-    plt.xlabel('RA')
-    plt.ylabel('Dec')
-    
-    plt.ylim(top, lower)
-    plt.xlim(right, left)
-
-    plt.show()
+    plt.plot
