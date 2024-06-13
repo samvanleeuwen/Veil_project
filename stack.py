63d9b1bfeda74d7c9a8751e2ae8c6ba47c83a9d5
MilanBom <milan.bom@student.uva.nl>
2024-06-13 16:40:57 +0200

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,74 +1,83 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  3 14:39:57 2024
+Created on Thu Jun 13 10:06:42 2024
 
-@author: heikamp
-
-
-start up script for astronomy image analysis with python
-
+@author: milan
 """
-# Import packages
-from astropy.io import fits  # to read in FITS files
+
+import numpy as np
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.visualization import ZScaleInterval
+import matplotlib.pyplot as plt
 import os  # os.path to manipulate file paths
 import glob  # finding pathnames (to search for certain fits files in folders and subfolders)
-import numpy as np  # math applied to arrays (important, no need to read pixel for pixel!)
-from matplotlib import pyplot as plt  # plot library
-from astropy.visualization import ZScaleInterval  # create minimum and maximum Z values for plotting
-from photutils.datasets import load_star_image
-from astropy.stats import sigma_clipped_stats
-from astropy.nddata import Cutout2D
-from astropy.wcs import WCS
-from twirl import find_peaks
-from photutils.aperture import CircularAperture
-from astropy.wcs.utils import proj_plane_pixel_scales
-from twirl import gaia_radecs
-from twirl.geometry import sparsify
-from twirl import compute_wcs
 
 
 
-# Import fits file
-data_im = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Stack van alle datasets/Stack Ha alle datasets.fit'
+# Path to the FITS file to process
+data_im = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Stack van alle datasets/GedownloadHa.fits'
 
 try:
-    hdu = fits.open(data_im)[0]
-    header = hdu.header
-    data = hdu.data
-    data, true_wcs = hdu.data, WCS(hdu.header)
-    mean, median, std = sigma_clipped_stats(data, sigma=3.0)
-    xy = find_peaks(data)[0:20]
-    
+    # Open the FITS file and extract data
+    with fits.open(data_im) as hdu_list:
+        hdu = hdu_list[0]
+        header = hdu.header
+        data = hdu.data
+        
+    true_wcs = WCS(header)
+    print(header)
+
+    # Plot the FITS image with WCS coordinates
+    plt.figure()
+    #ax = fig.add_subplot(projection=true_wcs)
+
+    # Apply ZScaleInterval to set the display range
     z = ZScaleInterval()
     z1, z2 = z.get_limits(data)
-    plt.imshow(data, vmin=z1, vmax=z2, cmap='gray')  # Display the main image using ZScaleInterval
-    plt.imshow(data, vmin=np.median(data), vmax=3 * np.median(data), cmap="gray", alpha=0.5)  # Overlay the background image
-    _ = CircularAperture(xy, r=10.0).plot(color="y")
-    fov = (data.shape * proj_plane_pixel_scales(true_wcs))[0]
-    center = true_wcs.pixel_to_world(*np.array(data.shape) / 2)
-    all_radecs = gaia_radecs(center, 1.2 * fov)
 
-    # we only keep stars 0.01 degree apart from each other
-    all_radecs = sparsify(all_radecs, 0.01)
-    # we only keep the 12 brightest stars from gaia
-    wcs = compute_wcs(xy, all_radecs[0:30], tolerance=10)
-    # plotting to check the WCS
-    radecs_xy = np.array(wcs.world_to_pixel_values(all_radecs))
-    plt.imshow(data, vmin=np.median(data), vmax=3 * np.median(data), cmap="gray", alpha=0.5)
-    _ = CircularAperture(radecs_xy, 5).plot(color="y", alpha=0.5)
+    x_center = header['CRVAL1']
+    y_center = header['CRVAL2']
+    
+    # Define the provided pixel scale in degrees
+    pixel_scale = 0.431 / 3600
+
+    left = x_center - (header['CRPIX1'] * pixel_scale)
+    lower = y_center - (header['CRPIX2'] * pixel_scale)
+    
+    right = left + (4096*pixel_scale)
+    top = lower + (4096*pixel_scale)
+    
+    extent_image = [left, right, lower, top]
+
+    plt.imshow(data, vmin=z1, vmax=z2, cmap='viridis', origin='lower', extent=(left, right, lower, top))
+    #plt.set_title('FITS Imagewith WCS Coordinates')
+    
+    plt.xlabel('RA')
+    plt.ylabel('Dec')
+    
+    plt.ylim(top ,lower)
+    plt.xlim(left, right)
+    # Show plot
     plt.show()
+    print(left)
+    print(right)
+    print(lower)
+    print(top)
+
 except FileNotFoundError:
     print(f"File {data_im} not found.")
+except ValueError as ve:
+    print(f"ValueError: {ve}")
 except Exception as e:
-    print(f"An error occurred while opening the FITS file: {e}")
+    print(f"An error occurred: {e}")
 
 # Import list of fits files:
 # Set path to calibration folder (path_in) and path to save files (path_out)
 path_in = "C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen"
 path_out = "C:/Uva/Jaar 1 dingen/Project/Stacks project/Stacks gecalibreerd met x-ray en röntgen/"
 
-file_list = glob.glob(os.path.join(path_in, '**', '*.fit*'), recursive=True)  # Search subfolders if recursive=True
+file_list = glob.glob(os.path.join(path_in, '**', '*.fits*'), recursive=True)  # Search subfolders if recursive=True
 
 # Debugging output to check file_list contents
 print(f"Files found: {file_list}")
@@ -79,45 +88,40 @@ sorted_list = sorted(file_list, key=os.path.getmtime)
 print(f"Sorted files: {sorted_list}")
 
 for getal in range(0,3):
-    hdu_list= fits.open(sorted_list[getal])[0]
+    getal = getal + 1
+    hdu_list= fits.open(file_list[getal])[0]
     header = hdu_list.header
     data, true_wcs = hdu_list.data, WCS(hdu_list.header)
-    mean, median, std = sigma_clipped_stats(data, sigma=3.0)
-    xy = find_peaks(data)[0:20]
 
+    # Plot the FITS image with WCS coordinates
+    plt.figure()
+    #ax = fig.add_subplot(projection=true_wcs)
+
+    # Apply ZScaleInterval to set the display range
     z = ZScaleInterval()
     z1, z2 = z.get_limits(data)
-    plt.imshow(data, vmin=z1, vmax=z2, cmap='gray')
-    plt.imshow(data, vmin=np.median(data), vmax=3 * np.median(data), cmap="grey")
-    _ = CircularAperture(xy, r=10.0).plot(color="y")
-    fov = (data.shape * proj_plane_pixel_scales(true_wcs))[0]
-    center = true_wcs.pixel_to_world(*np.array(data.shape) / 2)
-    all_radecs = gaia_radecs(center, 1.2 * fov)
+    
+    x_center = header['CRVAL1']
+    y_center = header['CRVAL2']
+    
+    # Define the provided pixel scale in degrees
+    pixel_scale = 1.76 / 3600
 
-    # we only keep stars 0.01 degree apart from each other
-    all_radecs = sparsify(all_radecs, 0.01)
-    # we only keep the 12 brightest stars from gaia
-    wcs = compute_wcs(xy, all_radecs[0:30], tolerance=10)
-    # plotting to check the WCS
-    radecs_xy = np.array(wcs.world_to_pixel_values(all_radecs))
-    plt.imshow(data, vmin=np.median(data), vmax=3 * np.median(data), cmap="grey", alpha=0.5)
-    _ = CircularAperture(radecs_xy, 5).plot(color="y", alpha=0.5)
+    left = x_center - (header['CRPIX1'] * pixel_scale)
+    lower = y_center - (header['CRPIX2'] * pixel_scale)
+    
+    right = left + (1024*pixel_scale)
+    top = lower + (1024*pixel_scale)
+    
+    extent_image = [left, right, lower, top]
+
+    plt.imshow(data, vmin=z1, vmax=z2, cmap='viridis', origin='lower', extent=(left, right, lower, top))
+    #plt.set_title('FITS Imagewith WCS Coordinates')
+    
+    plt.xlabel('RA')
+    plt.ylabel('Dec')
+    
+    plt.ylim(top, lower)
+    plt.xlim(right, left)
+
     plt.show()
-
-if len(sorted_list) > 1:
-    try:
-        hdu_list = fits.open(sorted_list[2])[0]  # 1 = 2nd item in list (index starts at 0)
-        header = hdu_list.header
-        data = hdu_list.data
-
-        # Simple manipulation and save fits file
-        data_transposed = data.T
-        data_sub = data_transposed - data_transposed  # Example manipulation
-        data_median = np.median(data)
-
-        hdu = fits.PrimaryHDU(data_sub, header=header)
-        hdu.writeto(os.path.join(path_out, "filename.fits"), overwrite=True)
-    except Exception as e:
-        print(f"An error occurred while processing the FITS file: {e}")
-else:
-    print("No FITS files found or not enough files in the specified directory.")
