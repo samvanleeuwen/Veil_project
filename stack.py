d672850f3025ea3f6825d7761b822f6c72f80c12
MilanBom <milan.bom@student.uva.nl>
2024-06-17 14:33:14 +0200

    Update stack.py

--- stack.py
+++ stack.py
@@ -5,55 +5,85 @@ Created on Fri Jun 14 10:58:45 2024
 @author: milan
 """
 
-import matplotlib.pyplot as plt
-from astropy.wcs import WCS
-from astropy.io import fits
 import numpy as np
-from astropy.visualization import ZScaleInterval
+from astropy.io import fits
+from astropy.wcs import WCS
 from reproject import reproject_interp
 from astropy.visualization import ZScaleInterval
+import matplotlib.pyplot as plt
 
-filename = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/GedownloadHa.fits'
+# Paths to the FITS files
+fits_files = [
+    'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/GedownloadHa.fits',
+    'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/GedownloadO3.fits',
+    'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/DSS1 2.fits',
+    'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/Wise 12 2.fits',
+    'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/PSPC2.0 2.fits'
+]
 
-# Path to the FITS files
-reference_fits = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/GedownloadHa.fits'  # FITS file with the desired WCS
-target_fits = 'C:/Uva/Jaar 1 dingen/Project/Stacks project/Filters x-ray en röntgen/GedownloadWise12.fits'     # FITS file to be aligned
+# Titles for each image
+titles = [
+    'Ha',
+    'OIII',
+    'DSS 1 red',
+    'Wise 12',
+    'PSPC 2.0'
+]
 
-# Open the reference FITS file
-with fits.open(reference_fits) as ref_hdu_list:
+# Fixed Box coordinates [(x1, y1, x2, y2)]
+boxes = [
+    (1120, 2584, 1936, 2912),
+    (2264, 304, 2696, 576),
+    (1608, 1648, 2040, 1944),
+    (121, 2790, 780, 3321)
+]
+
+# Define a function to plot the intensity profiles for each box in separate figures
+def plot_intensity_profiles(data_list, boxes):
+    """
+    Plots the intensity profiles of vertical boxes at specified positions.
+    """
+    colors = ['b', 'g', 'r', 'c', 'm']
+    
+    for box_idx, box in enumerate(boxes):
+        plt.figure(figsize=(8, 6))
+        
+        for data_idx, (data, title) in enumerate(zip(data_list, titles)):
+            reprojected_data, tgt_wcs = data
+            x1, y1, x2, y2 = box
+            if x2 <= reprojected_data.shape[1] and y2 <= reprojected_data.shape[0]:
+                box_data = reprojected_data[y1:y2, x1:x2]
+                intensity_profile = np.mean(box_data, axis=1)
+                normalized_intensity_profile = (intensity_profile - np.min(intensity_profile)) / (np.max(intensity_profile) - np.min(intensity_profile))
+                y_positions = np.arange(y1, y2)
+                plt.plot(y_positions, normalized_intensity_profile, color=colors[data_idx], label=f'{title}')
+            else:
+                print(f"Warning: Box {box} exceeds image dimensions for {title}")
+        
+        plt.xlabel('Pixel y-coordinate')
+        plt.ylabel('Normalized intensity')
+        plt.title(f'Intensity Profiles for Box {box_idx + 1}')
+        plt.legend()
+        plt.show()
+
+# Open the reference FITS file (first file in the list)
+with fits.open(fits_files[0]) as ref_hdu_list:
     ref_hdu = ref_hdu_list[0]
-    ref_data = ref_hdu.data
+    ref_data = ref_hdu.data.astype(float)  # Ensure data is float
     ref_wcs = WCS(ref_hdu.header)
 
-# Open the target FITS file
-with fits.open(target_fits) as tgt_hdu_list:
-    tgt_hdu = tgt_hdu_list[0]
-    tgt_data = tgt_hdu.data
-    tgt_wcs = WCS(tgt_hdu.header)
+# Initialize a list to store the reprojected data
+reprojected_data_list = []
 
-from reproject import reproject_interp
+# Reproject each FITS file to the WCS of the reference image
+for file, title in zip(fits_files, titles):
+    with fits.open(file) as tgt_hdu_list:
+        tgt_hdu = tgt_hdu_list[0]
+        tgt_data = tgt_hdu.data.astype(float)  # Ensure data is float
+        tgt_wcs = WCS(tgt_hdu.header)
+        
+        reprojected_data, footprint = reproject_interp((tgt_data, tgt_wcs), ref_wcs, shape_out=ref_data.shape)
+        reprojected_data_list.append((reprojected_data, tgt_wcs))
 
-# Reproject the target image to the WCS of the reference image
-reprojected_data, footprint = reproject_interp((tgt_data, tgt_wcs), ref_wcs, shape_out=ref_data.shape)
-
-# Apply ZScaleInterval to set the display range
-z = ZScaleInterval()
-ref_z1, ref_z2 = z.get_limits(ref_data)
-tgt_z1, tgt_z2 = z.get_limits(reprojected_data)
-
-# Plot the reference image
-fig, ax = plt.subplots(1, 2, figsize=(12, 6), subplot_kw={'projection': ref_wcs})
-ax[0].imshow(ref_data, vmin=ref_z1, vmax=ref_z2, cmap='gray', origin='lower')
-ax[0].set_title('Reference Image')
-ax[0].coords.grid(color='white', ls='solid')
-ax[0].set_xlabel('RA')
-ax[0].set_ylabel('Dec')
-
-# Plot the reprojected target image
-ax[1].imshow(reprojected_data, vmin=tgt_z1, vmax=tgt_z2, cmap='gray', origin='lower')
-ax[1].set_title('Reprojected Target Image')
-ax[1].coords.grid(color='white', ls='solid')
-ax[1].set_xlabel('RA')
-ax[1].set_ylabel('Dec')
-
-plt.show()
+# Plot intensity profiles for each box in separate figures
+plot_intensity_profiles(reprojected_data_list, boxes)
